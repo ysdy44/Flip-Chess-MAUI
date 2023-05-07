2f8ccb50292ff69aaa7897ac6ad641ff347efd79
ysdy44 <ysdy44@outlook.com>
2023-05-07 18:21:40 +0800

    Elements Add ToggleButton

---
+++ Flip_Chess.Elements/ToggleButton.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace Flip_Chess.Elements
+{
+    public sealed class ToggleButton : Button
+    {
+        #region DependencyProperty
+
+        public bool IsChecked
+        {
+            get => (bool)base.GetValue(IsCheckedProperty);
+            set => base.SetValue(IsCheckedProperty, value);
+        }
+        public static readonly BindableProperty IsCheckedProperty = BindableProperty.Create(nameof(IsChecked), typeof(bool), typeof(CommandListView), false);
+
+        #endregion
+
+        public ToggleButton( )
+        {
+            base.Clicked += (s, e) =>
+            {
+                this.IsChecked = !this.IsChecked;
+            };
+        }
+    }
+}
