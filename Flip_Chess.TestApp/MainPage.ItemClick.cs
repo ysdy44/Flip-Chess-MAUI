f158ffae39ce13d8327179a742352666874027aa
ysdy44 <ysdy44@outlook.com>
2023-05-07 18:19:08 +0800

    TestApp Add MainPage

---
+++ Flip_Chess.TestApp/MainPage.ItemClick.cs
@@ -0,0 +1,59 @@
+﻿using Flip_Chess.Chesses;
+using Flip_Chess.Chesses.Extensions;
+using Microsoft.Maui.Controls;
+
+namespace Flip_Chess.TestApp
+{
+    partial class MainPage
+    {
+        public void ItemClick(History history)
+        {
+            if (history.Distance is HistoryDistance.Others) return;
+
+            this.Timer.Stop();
+            this.Timer.Start();
+
+            this.Historian.Add(history);
+
+            switch ((HistoryAction)history.Distance)
+            {
+                case HistoryAction.Noway:
+                    {
+                        this.Randoms.Home();
+                        this.Randoms.Random();
+
+                        this.Collection.Clear();
+                        this.Chesses.Copy(this.Collection);
+
+                        this.Historian.Clear();
+                    }
+                    break;
+                case HistoryAction.Flip:
+                    {
+                        // Info
+                        int index = this.Collection.IndexOf(history.Y, history.X);
+                        ChessType random = this.Randoms[index].Type;
+                        this.Collection[0, history.Y, history.X] = random;
+
+                        // UI
+                        this.Chesses.Copy(this.Collection);
+                    }
+                    break;
+                case HistoryAction.Capture:
+                    {
+                        // Info
+                        ChessType fromType = this.Collection[0, history.Y1, history.X1];
+
+                        this.Collection[0, history.Y1, history.X1] = ChessType.Deaded;
+                        this.Collection[0, history.Y2, history.X2] = fromType;
+
+                        // UI
+                        this.Chesses.Copy(this.Collection);
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
--- Flip_Chess.TestApp/MainPage.xaml.cs
+++ Flip_Chess.TestApp/MainPage.xaml.cs
@@ -1,13 +1,79 @@
-﻿using Microsoft.Maui.Controls;
+﻿using Flip_Chess.Chesses;
+using Flip_Chess.Chesses.AutoAIs;
+using Flip_Chess.Chesses.Extensions;
+using Flip_Chess.TestApp.Models;
+using Microsoft.Maui.Controls;
+using System.Collections.Generic;
+using System.Timers;
 
 namespace Flip_Chess.TestApp
 {
     public partial class MainPage : ContentPage
     {
+        readonly Timer Timer = new Timer
+        {
+            Interval = 1000
+        };
+
+        // History
+        public int Step => this.Historian.Count;
+        public IList<History> Historian { get; } = new List<History>();
+
+        // Collection
+        public ChessType[,,] Collection { get; } = new ChessType[1024, 8, 4]; // Sertings
+        public Chess[] Chesses { get; } = new Chess[8 * 4]
+        {
+            new Chess(), new Chess(), new Chess(), new Chess(),
+            new Chess(), new Chess(), new Chess(), new Chess(),
+            new Chess(), new Chess(), new Chess(), new Chess(),
+            new Chess(), new Chess(), new Chess(), new Chess(),
+            new Chess(), new Chess(), new Chess(), new Chess(),
+            new Chess(), new Chess(), new Chess(), new Chess(),
+            new Chess(), new Chess(), new Chess(), new Chess(),
+            new Chess(), new Chess(), new Chess(), new Chess(),
+        };
+        public Chess[] Randoms { get; } = new Chess[8 * 4] // Sertings
+        {
+            new Chess(), new Chess(), new Chess(), new Chess(),
+            new Chess(), new Chess(), new Chess(), new Chess(),
+            new Chess(), new Chess(), new Chess(), new Chess(),
+            new Chess(), new Chess(), new Chess(), new Chess(),
+            new Chess(), new Chess(), new Chess(), new Chess(),
+            new Chess(), new Chess(), new Chess(), new Chess(),
+            new Chess(), new Chess(), new Chess(), new Chess(),
+            new Chess(), new Chess(), new Chess(), new Chess(),
+        };
+
+        //@Construct
         public MainPage()
         {
             this.InitializeComponent();
             base.BindingContext = this;
+            BindableLayout.SetItemsSource(this.ItemsControl, this.Chesses);
+
+            this.Randoms.Home();
+            this.Randoms.Random();
+
+            this.Timer.Elapsed += async (s, e) =>
+            {
+                if (this.Step.IsRed())
+                {
+                    this.Timer.Stop();
+                    this.ItemClick(await System.Threading.Tasks.Task.Run(this.FindRed));
+                    this.Timer.Start();
+                }
+                else if (this.Step.IsBlack())
+                {
+                    this.Timer.Stop();
+                    this.ItemClick(await System.Threading.Tasks.Task.Run(this.FindBlack));
+                    this.Timer.Start();
+                }
+            };
+            this.Timer.Start();
         }
+
+        // AutoAi
+        private History FindRed() => new RedAutoAICollection(this.Collection).FindAutoAI(this.Collection);
+        private History FindBlack() => new BlackAutoAICollection(this.Collection).FindAutoAI(this.Collection);
     }
 }
