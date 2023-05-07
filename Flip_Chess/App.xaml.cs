384f1c540e83dacc925044050d528a10ba9384a9
ysdy44 <ysdy44@outlook.com>
2023-05-07 18:25:06 +0800

    App Add Fonts

--- Flip_Chess/App.xaml.cs
+++ Flip_Chess/App.xaml.cs
@@ -7,6 +7,10 @@ namespace Flip_Chess
 {
     public partial class App : Application
     {
+        //@Const
+        internal const int Width = 4;
+        internal const int Height = 8;
+
         public App()
         {
             this.InitializeComponent();
@@ -28,6 +32,18 @@ namespace Flip_Chess
 
         public static void ConfigureFonts(IFontCollection fonts)
         {
+            // 9 Icons:
+            // <FontImageSource x:Name="Continue" Glyph="&#xE102;" FontFamily="Segoe UI Symbol"/>
+            // <FontImageSource x:Name="Pause" Glyph="&#xE103;" FontFamily="Segoe UI Symbol"/>
+            // <FontImageSource x:Name="Next" Glyph="&#xE111;" FontFamily="Segoe UI Symbol"/>
+            // <FontImageSource x:Name="Computer" Glyph="&#xE115;" FontFamily="Segoe UI Symbol"/>
+            // <FontImageSource x:Name="Mode" Glyph="&#xE138;" FontFamily="Segoe UI Symbol"/>
+            // <FontImageSource x:Name="Players" Glyph="&#xE13d;" FontFamily="Segoe UI Symbol"/>
+            // <FontImageSource x:Name="Restart" Glyph="&#xE149;" FontFamily="Segoe UI Symbol"/>
+            // <FontImageSource x:Name="UnFullScreen" Glyph="&#xE1d8;" FontFamily="Segoe UI Symbol"/>
+            // <FontImageSource x:Name="FullScreen" Glyph="&#xE1d9;" FontFamily="Segoe UI Symbol"/>
+            fonts.AddFont("Segoe UI Symbol Regular.ttf", "Segoe UI Symbol");
+
             fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
             fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
         }
